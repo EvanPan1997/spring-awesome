b05e87739dd81718506d238ebe3fcbc41e106889
Evan.Pan <panyf_97@163.com>
2024-09-25 10:16:52 +0800

    update: 注释

--- spring-boot-demo/src/main/java/com/example/listener/RequestHandledListener.java
+++ spring-boot-demo/src/main/java/com/example/listener/RequestHandledListener.java
@@ -4,6 +4,7 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.support.RequestHandledEvent;
 
+// 请求处理监听器(不适用于系统日志)
 @Component
 public class RequestHandledListener implements ApplicationListener<RequestHandledEvent> {
 
