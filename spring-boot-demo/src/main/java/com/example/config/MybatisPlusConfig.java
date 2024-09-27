23d6271148f56b20802727a6053618c0c5eac4f9
Evan.Pan <panyf_97@163.com>
2024-09-27 16:13:38 +0800

    add: 添加mybatis-plus分页处理配置

---
+++ spring-boot-demo/src/main/java/com/example/config/MybatisPlusConfig.java
@@ -0,0 +1,29 @@
+package com.example.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.autoconfigure.MybatisPlusProperties;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+/** Mybatis-Plus 配置类 */
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return mybatisPlusInterceptor;
+    }
+
+    @Primary
+    @Bean
+    @ConfigurationProperties("mybatis-plus")
+    public MybatisPlusProperties mybatisPlusProperties() {
+        return new MybatisPlusProperties();
+    }
+}
