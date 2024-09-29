6c5847c992b0a406d384c7359f7736d553c2dec9
Evan.Pan <panyf_97@163.com>
2024-09-29 16:01:35 +0800

    update: 修改返回

--- spring-boot-demo/src/main/java/com/example/controller/TestController.java
+++ spring-boot-demo/src/main/java/com/example/controller/TestController.java
@@ -1,8 +1,10 @@
 package com.example.controller;
 
 import com.example.utils.ResponseResult;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
