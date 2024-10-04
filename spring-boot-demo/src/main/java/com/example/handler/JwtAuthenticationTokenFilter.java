46a8c72d2a3d5501dd4fd20c1bc906490f6f40d4
Evan.Pan <panyf_97@163.com>
2024-10-04 10:29:26 +0800

    update: JwtAuthenticationTokenFilter中加入redis操作

--- spring-boot-demo/src/main/java/com/example/handler/JwtAuthenticationTokenFilter.java
+++ spring-boot-demo/src/main/java/com/example/handler/JwtAuthenticationTokenFilter.java
@@ -1,16 +1,22 @@
 package com.example.handler;
 
+import com.example.entity.LoginUser;
 import com.example.utils.JwtUtils;
 import io.jsonwebtoken.Claims;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.annotation.Resource;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Objects;
 
 /**
  * 自定义一个过滤器，去获取请求头中的 token，对 token 进行解析取出其中的 userId,
@@ -19,8 +25,16 @@ import java.io.IOException;
  */
 @Component
 public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
+
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
+
+    private static final String PREFIX_LOGIN_KEY = "login:";
+
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
         String token = request.getHeader("X-ACCESS-TOKEN");
         if (!StringUtils.hasText(token)) {
             filterChain.doFilter(request, response);
@@ -36,6 +50,16 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
             throw new RuntimeException("token非法");
         }
 
+        String redisKey = PREFIX_LOGIN_KEY + userId;
+        LoginUser loginUser = (LoginUser) redisTemplate.opsForValue().get(redisKey);
+        if (Objects.isNull(loginUser)) {
+            throw new RuntimeException("用户未登录");
+        }
+
+        // 获取权限信息封装到Authentication中
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginUser, null, loginUser.getAuthorities());
+        // 存入 SecurityContextHolder
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
 
         filterChain.doFilter(request, response);
     }
