d7e0291e5d665058a82889924c81f89f74414487
Evan.Pan <panyf_97@163.com>
2024-10-06 17:08:23 +0800

    update: 调整返回对象

--- spring-boot-demo/src/main/java/com/example/impl/security/UserDetailsServiceImpl.java
+++ spring-boot-demo/src/main/java/com/example/impl/security/UserDetailsServiceImpl.java
@@ -1,32 +1,37 @@
 package com.example.impl.security;
 
+import com.example.entity.LoginUser;
 import com.example.entity.SystemUser;
 import com.example.service.SystemUserService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
     @Resource
     private SystemUserService systemUserService;
-    @Resource
-    private PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         SystemUser systemUser = systemUserService.getUserByUserId(username);
-        if (!ObjectUtils.isEmpty(systemUser)) {
-            return org.springframework.security.core.userdetails.User.withUsername(username)
-                    .password(passwordEncoder.encode(systemUser.getPassword()))
-                    .authorities("ROLE_USER")
-                    .build();
+//        if (!ObjectUtils.isEmpty(systemUser)) {
+//            return org.springframework.security.core.userdetails.User.withUsername(username)
+//                    .password(passwordEncoder.encode(systemUser.getPassword()))
+//                    .authorities("ROLE_USER")
+//                    .build();
+//        }
+        if (Objects.isNull(systemUser)) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
         }
-        throw new UsernameNotFoundException("User not found with username: " + username);
+
+        List<String> list = new ArrayList<>();
+        return new LoginUser(systemUser, list);
     }
 }
