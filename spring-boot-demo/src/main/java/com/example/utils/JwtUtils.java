dbbc329c2944e509bee10aee7be854e0a2ae5bd8
Evan.Pan <panyf_97@163.com>
2024-10-06 17:05:56 +0800

    update: JWT工具类中字段名调整

--- spring-boot-demo/src/main/java/com/example/utils/JwtUtils.java
+++ spring-boot-demo/src/main/java/com/example/utils/JwtUtils.java
@@ -3,29 +3,37 @@ package com.example.utils;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.TextCodec;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 public class JwtUtils {
-    private static final String secretKey = "test_key";
+    private static final String SECRET_KEY = "test";
+    private static final String PROJECT_NAME = "spring-awesome";
 
-    private static final long time = 86400000;
+    private static final long TIME = 86400000;
 
     public static String generateToken(String userId) {
         Date now = new Date();
+        Date exp = new Date(now.getTime() + TIME);
+
+        byte[] bytes = SECRET_KEY.getBytes(StandardCharsets.UTF_8);
+//        String encode = TextCodec.BASE64.encode(bytes);
+
         return Jwts.builder()
                 .setSubject(userId)
                 .setIssuer("evan")
                 .setIssuedAt(now)
-                .setExpiration(new Date(now.getTime() + time))
-                .claim("project", "spring-awesome") // 临时参数
-                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .setExpiration(exp)
+                .claim("project", PROJECT_NAME) // 临时参数
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
                 .compact();
     }
 
     public static Claims parseJWT(String jwt) {
         return Jwts.parser()
-                .setSigningKey(secretKey)
+                .setSigningKey(SECRET_KEY)
                 .parseClaimsJws(jwt)
                 .getBody();
     }
